8c35f3a42909582cbc3ea3b46f11e26d22310819
Nika <nika@w-up.ru>
2023-10-23 18:49:56 +0300

    bug fixes

--- stats/service/src/main/java/ru/practicum/stats/service/ErrorHandler.java
+++ stats/service/src/main/java/ru/practicum/stats/service/ErrorHandler.java
@@ -1,6 +1,7 @@
 package ru.practicum.stats.service;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -12,17 +13,15 @@ public class ErrorHandler {
 
     @ExceptionHandler
     @ResponseStatus(HttpStatus.BAD_REQUEST)
-    ErrorResponse handleValidationException(final ValidationException e) {
+    public ErrorResponse handleValidationException(final ValidationException e) {
         return new ErrorResponse(e.getMessage());
     }
 
 
     @ExceptionHandler
-    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-    public ErrorResponse handleThrowable(final Throwable e) {
-        return new ErrorResponse(
-                "An unexpected error occurred." + e.getMessage()
-        );
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorResponse handleMethodArgumentNotValidException(final MethodArgumentNotValidException e) {
+        return new ErrorResponse(e.getMessage());
     }
 
     public class ErrorResponse {
